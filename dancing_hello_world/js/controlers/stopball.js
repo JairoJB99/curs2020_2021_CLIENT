4035f9373c53c7232b33097c70c51c10772f65bf
JairoJB99 <jairocnx@gmail.com>
2020-09-23 21:35:29 +0200

    Imports & Exports v1.3

--- dancing_hello_world/js/controlers/stopball.js
+++ dancing_hello_world/js/controlers/stopball.js
@@ -1,7 +1,7 @@
 'use strict'
 import app from './../main'
 
-export function stopBall() {
+export default function stopBall() {
     document.body.onkeyup = function(e){
         if(e.keyCode == 32){
             app.toggle();
