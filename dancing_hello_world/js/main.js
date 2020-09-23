c44c45445a6e3c290fb51035014fca5416849318
JairoJB99 <jairocnx@gmail.com>
2020-09-23 23:56:18 +0200

    Imports & Exports v1.16

--- dancing_hello_world/js/main.js
+++ dancing_hello_world/js/main.js
@@ -1,50 +1,51 @@
 'use strict'
 import docReady from './core/core'
 
- export let inicia = (function() {
+export let inicia = (function () {
     // DOM is loaded and ready for manipulation here
-    let speed = 15; //1 to 100
-    let incX = speed * (Math.round(Math.random())?1:-1);
-    let incY = speed * (Math.round(Math.random())?1:-1);
-    let stateApp="run"
+
+    let stateApp = "run"
     let myApp;
-    
-    let dw = function(el){
-    
-        // declaramos x y (antes no estaban)
-        let x =  el.style.left?parseInt(el.style.left,10):350;
-        let y =  el.style.top?parseInt(el.style.top,10):400;
 
-        el.style.left =  x + incX +"px";
-        el.style.top = y + incY+"px";
-    
-        //Detect if we reach X coordinates limits
-        if (((x+incX) > (window.innerWidth-40)) || ((x+incX)<=0))
-            incX = (-1)*incX;
-       
-        //Detect if we reach Y coordinates limits
-        if (((y+incY) > (window.innerHeight-40)) || ((y+incY) <= 0))
-            incY = (-1)*incY;
+    let dw = function (el) {
+        let speed = 15; //1 to 100
+        let incX = speed * (Math.round(Math.random()) ? 1 : -1);
+        let incY = speed * (Math.round(Math.random()) ? 1 : -1);
 
-        return dw;
+        let aux = function () {
+            // declaramos x y (antes no estaban)
+            let x = el.style.left ? parseInt(el.style.left, 10) : 350;
+            let y = el.style.top ? parseInt(el.style.top, 10) : 400;
+
+            el.style.left = x + incX + "px";
+            el.style.top = y + incY + "px";
+
+            //Detect if we reach X coordinates limits
+            if (((x + incX) > (window.innerWidth - 40)) || ((x + incX) <= 0))
+                incX = (-1) * incX;
+
+            //Detect if we reach Y coordinates limits
+            if (((y + incY) > (window.innerHeight - 40)) || ((y + incY) <= 0))
+                incY = (-1) * incY;
+
+        };
+        return aux;
     };
-    let aux = document.getElementById("ball");
-    console.log(aux.innerHTML);
 
-    let start = function(){
+    let start = function () {
         console.log("Arrancando");
-        stateApp= "run";
-        myApp=setInterval(dw(aux),50);
+        stateApp = "run";
+        myApp = setInterval(dw(document.getElementById("ball")), 50);
     }
-    let stop = function(){
+    let stop = function () {
         console.log("Parando");
-        stateApp= "stop";
+        stateApp = "stop";
         clearInterval(myApp);
     }
-    let toggle = function(){
-        (stateApp==="run") ? stop(): start()
+    let toggle = function () {
+        (stateApp === "run") ? stop() : start()
     }
-    return { start: start, toggle: toggle}
+    return { start: start, toggle: toggle }
 })();
 
 docReady(inicia.start);
