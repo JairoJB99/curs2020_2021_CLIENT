03c18a57533715fe686dfd71c694589a2033db84
JairoJB99 <jairocnx@gmail.com>
2020-09-23 21:34:40 +0200

    Imports & Exports v1.2

--- dancing_hello_world/js/core/core.js
+++ dancing_hello_world/js/core/core.js
@@ -1,4 +1,4 @@
- export function docReady(fn) {
+ export default function docReady(fn) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
         // call on next available tick
