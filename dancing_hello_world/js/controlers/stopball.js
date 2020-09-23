7c5720cb8f87d228aae985d9ca4c6748b8e62f87
JairoJB99 <jairocnx@gmail.com>
2020-09-24 00:25:53 +0200

    Imports & Exports v1.19

--- dancing_hello_world/js/controlers/stopball.js
+++ dancing_hello_world/js/controlers/stopball.js
@@ -1,11 +1,11 @@
 'use strict'
 import {inicia} from './../main'
 
-export default function stopBall() {
+export default (function stopBall() {
     document.body.onkeyup = function(e){
         if(e.keyCode == 32){
             console.log("pulsado");
             inicia.toggle();
         }
     }
-}
+})();
