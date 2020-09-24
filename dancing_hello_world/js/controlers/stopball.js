65b766357d0b677bc0f51e72ddfd182706e0e3d4
JairoJB99 <jairocnx@gmail.com>
2020-09-24 16:44:48 +0200

    Imports & Exports v1.35

--- dancing_hello_world/js/controlers/stopball.js
+++ dancing_hello_world/js/controlers/stopball.js
@@ -1,7 +1,7 @@
 'use strict'
-import inicia from '../main.js'
+import {inicia} from '../main.js'
 
-export default document.body.onkeyup = function(e){
+document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         console.log("pulsada");
         inicia.toggle();
