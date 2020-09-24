e963dd2e3d7a02b01579442fecaa378b1e97cdc9
JairoJB99 <jairocnx@gmail.com>
2020-09-24 17:01:13 +0200

    Imports & Exports vFinal4

--- dancing_hello_world/js/core/core.js
+++ dancing_hello_world/js/core/core.js
@@ -1,5 +1,5 @@
  'use strict'
-export default function docReady(fn) {
+ function docReady(fn) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
         // call on next available tick
@@ -9,4 +9,4 @@ export default function docReady(fn) {
         document.addEventListener("DOMContentLoaded", fn); //
     }
 }    
-
+export {docReady}
